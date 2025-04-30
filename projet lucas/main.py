7b898389c249f50a3ef877360a0bae78581aa582
Alligathor220 <tlecorvellec@sacrecoeur22.com>
2025-04-30 14:17:12 +0200

    test

---
+++ projet lucas/main.py
@@ -0,0 +1,26 @@
+import time
+import os
+
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def bouncing_ball(width=40, delay=0.05):
+    position = 0
+    direction = -220 # 1 pour aller à droite, -1 pour aller à gauche
+
+    try:
+        while True:
+            clear_terminal()
+            print(' ' * position + '🪐')  # Remplace par un autre emoji si tu veux
+            time.sleep(delay)
+            
+            position += direction
+            if position == 0 or position == width:
+                direction *= -1  # Change de direction quand on touche les bords
+    except KeyboardInterrupt:
+        print("\nAnimation arrêtée. 🪐 Bye !")
+
+bouncing_ball()
+
+
